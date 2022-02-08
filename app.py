3b1760dd601955cdd0e00d6d76bf9d2cba955a5c
unknown <powerfulsona2215@gmail.com>
2022-02-08 17:50:18 -0500

    first commit

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 import os
 from flask import  Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+# import additional libraries below
 import cv2
 import numpy as np
+
+
 
 app = Flask(__name__)
 
@@ -12,39 +15,61 @@ app = Flask(__name__)
 def load_form():
     return render_template('upload.html')
 
-
 # Function to upload image and redirect to new webpage
 @app.route('/gray', methods=['POST'])
 def upload_image():
     file = request.files['file']
     filename = secure_filename(file.filename)
-
+    # write the read and write function on image below 
     file_data = make_grayscale(file.read())
     with open(os.path.join('static/', filename),
-              'wb') as f:
-        f.write(file_data)
+    		 'wb') as f:
+   		f.write(file_data)
+
+
+        # ends here
 
     display_message = 'Image successfully uploaded and displayed below'
     return render_template('upload.html', filename=filename, message = display_message)
 
 
+# Write the make_grayscale() function below
 
 def make_grayscale(input_image):
 
-    image_array = np.fromstring(input_image, dtype='uint8')
-    print('Image Array:',image_array)
+	image_array = np.fromstring(input_image, dtype='uint8')
+	print('Image Array:',image_array)
 
-    # decode the array into an image
-    decode_array_to_img = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
-    print('Decode values of Image:', decode_array_to_img)
+	decode_array_to_img = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
+	print('Decode values of Image:', decode_array_to_img)
 
-    # Make grayscale
-    converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_RGB2GRAY)
-    status, output_image = cv2.imencode('.PNG', converted_gray_img)
-    print('Status:',status)
+	converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_RGB2GRAY)
+	status, output_image = cv2.imencode('.PNG', converted_gray_img)
+	print('Status:', status)
 
-    return output_image
+	return output_image
 
+@app.route('/display/<filename>')
+def display_image(filename):
+	return redirect(url_for('static', filename=filename))
+
+
+if __name__ == '__main__':
+	app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# make_grayscale() function ends above
 
 @app.route('/display/<filename>')
 def display_image(filename):
